f19150bf692a0f9d7d422ccf5d3f299a4d856778
Romes Heriberto <romes.heriberto@gmail.com>
2023-10-04 19:34:46 -0300

    adjustments

--- atividades/exercicio05.cs
+++ atividades/exercicio05.cs
@@ -14,3 +14,27 @@ Dicas:
 Use os métodos ToUpper(), ToLower(), Length, Trim() e conversões para manipular as strings.
 Use a classe Convert para converter a idade fornecida em um número inteiro.
 Lembre-se de que o ano de nascimento pode ser calculado subtraindo a idade do ano atual (2023). */
+
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Digite seu nome completo:");
+        string nomeCompleto = Console.ReadLine();
+
+        Console.WriteLine("Digite sua idade:");
+        string idade = Console.ReadLine();
+
+        Console.WriteLine($"Nome completo em maiúsculas: {nomeCompleto.ToUpper()}");
+        Console.WriteLine($"Nome completo em minúsculas: {nomeCompleto.ToLower()}");
+        Console.WriteLine($"Número de caracteres no nome completo: {nomeCompleto.Trim().Length}");
+
+        int idadeInteiro = Convert.ToInt32(idade);
+        int anoNascimento = 2023 - idadeInteiro;
+
+        Console.WriteLine($"Ano de nascimento: {anoNascimento}");
+        Console.WriteLine($"Idade em que fará 100 anos: {100 - idadeInteiro}");
+    }
+}
---
+++ material_de_aula/aula6.cs
@@ -0,0 +1,18 @@
+/* Sistema Básico de Gerenciamento de Livros
+Objetivo: Criar um sistema simples para gerenciar uma lista de livros, onde cada livro possui um título, autor e ISBN (Número Padrão Internacional de Livro).
+
+Definição da Classe Livro:
+
+Título
+Autor
+ISBN
+Métodos da Classe Livro:
+
+ExibirDetalhes(): Exibirá os detalhes do livro no console.
+Definição da Classe Biblioteca:
+
+Lista de Livros
+Métodos da Classe Biblioteca:
+
+AdicionarLivro(Livro livro): Adiciona um novo livro à lista.
+ExibirLivros(): Exibe os detalhes de todos os livros na biblioteca. */
